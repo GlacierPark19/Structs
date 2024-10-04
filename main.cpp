18de8309393c7e91c65317588e747955007324d1
Michael <ThisIsAnEmail@chapman-tech.net>
2024-10-04 11:37:21 -0700

    Ignoring the gitignore to gitignore the ignore

--- main.cpp
+++ main.cpp
@@ -13,4 +13,14 @@ struct customerReccord
 
 int main()
 {
+    vector<customerReccord *> bankCustomers;
+    customerReccord *customer1 = nullptr;
+    customer1 = new customerReccord;
+    customer1->customerName = "John Doe";
+    customer1->accountBalance = 1000.00;
+    customer1->accountNumber = 123456789;
+    customer1->isAccountOverdrawn = false;
+    bankCustomers.push_back(customer1);
+
+    cout << "Customer Name: " << customer1->customerName << endl;
 }
